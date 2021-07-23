2a073711c1d11a31a1212e2fb2592d050ed8f254
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-23 13:02:19 -0400

    Update pull.sh

--- 2.6.3/pull.sh
+++ 2.6.3/pull.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 singularity pull https://depot.galaxyproject.org/singularity/prodigal%3A2.6.3--h516909a_2
 mv -v prodigal%3A2.6.3--h516909a_2 singularity-prodigal-2.6.3.sif
